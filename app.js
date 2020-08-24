b3432496b545b1a7a80c66e3c0947da2593e4883
Toon Bosmans <toonbosmans@Toon.local>
2020-08-24 12:32:27 -0500

    able to pull the data from API that I need

--- app.js
+++ app.js
@@ -8,7 +8,18 @@
 
 // => working API!!
 
+//response is JSON with array per book item;
+//access title: .items[0].volumeInfo.title
+//access author: .items[0].volumeInfo.authors[0]
+
 $.ajax({
     url: 'https://www.googleapis.com/books/v1/users/112452514880183692834/bookshelves/1001/volumes'
-}).then(response => console.log(response));
+}).then((response) => {
+    console.log(response.items[0].volumeInfo.title);
+    console.log(response.items[0].volumeInfo.authors[0]);
+    console.log(response.items[1].volumeInfo.title);
+    console.log(response.items[1].volumeInfo.authors[0]);
+    const image = response.items[1].volumeInfo.imageLinks.thumbnail;
+    $('#book').append($(`<img src=${image}>`));
+})
 
