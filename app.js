884986e69b85904bea141a5302f95f216f3114bf
Toon Bosmans <toonbosmans@Toon.local>
2020-08-25 19:30:00 -0500

    set up html template and basic JS for the carousel

--- app.js
+++ app.js
@@ -11,15 +11,49 @@
 //response is JSON with array per book item;
 //access title: .items[0].volumeInfo.title
 //access author: .items[0].volumeInfo.authors[0]
-
+/*
 $.ajax({
     url: 'https://www.googleapis.com/books/v1/users/112452514880183692834/bookshelves/1001/volumes'
 }).then((response) => {
-    console.log(response.items[0].volumeInfo.title);
-    console.log(response.items[0].volumeInfo.authors[0]);
-    console.log(response.items[1].volumeInfo.title);
-    console.log(response.items[1].volumeInfo.authors[0]);
-    const image = response.items[1].volumeInfo.imageLinks.thumbnail;
-    $('#book').append($(`<img src=${image}>`));
+    //console.log(response.items[0].volumeInfo.title);
+    //console.log(response.items[0].volumeInfo.authors[0]);
+    //console.log(response.items[1].volumeInfo.title);
+    //console.log(response.items[1].volumeInfo.authors[0]);
+    //const image = response.items[1].volumeInfo.imageLinks.thumbnail;
+    //$('#book').append($(`<img src=${image}>`));
 })
+
+*/
+////CAROUSEL///
+//////////////
+
+let currentImgIndex = 0;
+const highestIndex = $('#carousel-images').children().length - 1;
+
+$("#next").on("click", () => {
+    $('#carousel-images').children().eq(currentImgIndex).css('display', 'none');
+    if (currentImgIndex < highestIndex) {
+        currentImgIndex++ 
+    } else {
+        currentImgIndex = 0;
+    }
+
+    $('#carousel-images').children().eq(currentImgIndex).css('display', 'block');
+
+
+});
+
+$("#previous").on("click", () => {
+    $('#carousel-images').children().eq(currentImgIndex).css('display', 'none');
+    if (currentImgIndex > 0) {
+        currentImgIndex-- 
+    } else {
+        currentImgIndex = highestIndex;
+    }
+
+    $('#carousel-images').children().eq(currentImgIndex).css('display', 'block');
+
+
+});
+
 
