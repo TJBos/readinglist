284ffdfdabe0b77db2de1b7d507874073a40bc39
Toon Bosmans <toonbosmans@Toon.local>
2020-08-26 19:54:23 -0500

    added event listener to the category divs

--- app.js
+++ app.js
@@ -7,47 +7,46 @@
 
 //https://www.googleapis.com/books/v1/users/112452514880183692834/bookshelves/1001/volumes
 
-// => working API!!
+//1001 is "software"
+//1002 is "science fiction"
+//1003 is non-fiction
+//1004 is fiction
+
+// => working AP for an example bookshelf
+//will make 4 bookshelves now with different books in it. 
 
 //response is JSON with array per book item;
 //access title: .items[0].volumeInfo.title
 //access author: .items[0].volumeInfo.authors[0]
 
+$('.genre').on("click", (event) => {
 
-
-$.ajax({
-    url: 'https://www.googleapis.com/books/v1/users/112452514880183692834/bookshelves/1001/volumes'
-}).then((response) => {
+    const category = $(event.target).attr('id');
     
-    for (item of response.items) {
-        const $newDiv = $('<div class="book">');
-        const image = item.volumeInfo.imageLinks.thumbnail;
-        $newDiv.append($(`<img src=${image}>`));
-        const title = item.volumeInfo.title;
-        const author = item.volumeInfo.authors[0];
-        const description = item.volumeInfo.description;
-        const $newBookInfoDiv = $('<div class="book-info">');
-        $newBookInfoDiv.append($('<h3>').text(title))
-                       .append($('<h4>').text(author))
-                       .append($('<p>').text(description));
+    $.ajax({
+        url: `https://www.googleapis.com/books/v1/users/112452514880183692834/bookshelves/${category}/volumes`
+    }).then((response) => {
+        
+        for (item of response.items) {
+            const $newDiv = $('<div class="book">');
+            const image = item.volumeInfo.imageLinks.thumbnail;
+            $newDiv.append($(`<img src=${image}>`));
+            const title = item.volumeInfo.title;
+            const author = item.volumeInfo.authors[0];
+            const description = item.volumeInfo.description;
+            const $newBookInfoDiv = $('<div class="book-info">');
+            $newBookInfoDiv.append($('<h3>').text(title))
+                           .append($('<h4>').text(author))
+                           .append($('<p>').text(description));
+    
+            $newDiv.append($newBookInfoDiv);
+            $("#carousel-images").append($newDiv);
+            highestIndex++
+        }    
+        
+    })   
 
-        $newDiv.append($newBookInfoDiv);
-        $("#carousel-images").append($newDiv);
-        highestIndex++
-    }    
-    /*console.log(response.items[0].volumeInfo.title);
-    console.log(response.items[0].volumeInfo.authors[0]);
-    console.log(response.items[1].volumeInfo.title);
-    console.log(response.items[1].volumeInfo.authors[0]);
-    const image1 = response.items[0].volumeInfo.imageLinks.thumbnail;
-    $('#first').append($(`<img src=${image1}>`));
-    const image2 = response.items[1].volumeInfo.imageLinks.thumbnail;
-    $('#second').append($(`<img src=${image2}>`));
-    const image3 = response.items[2].volumeInfo.imageLinks.thumbnail;
-    $('#third').append($(`<img src=${image3}>`));
-    */ 
-
-})   
+})
 
 ////CAROUSEL///
 //////////////
